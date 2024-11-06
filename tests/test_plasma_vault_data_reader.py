1fb13bdd85cf48d5aad8906de4df62834cbacf98
amydlarz <amydlarz@gmail.com>
2024-11-06 14:50:56 +0100

    fix test_plasma_vault_data_reader

--- tests/test_plasma_vault_data_reader.py
+++ tests/test_plasma_vault_data_reader.py
@@ -2,10 +2,9 @@ import os
 
 from web3 import Web3, HTTPProvider
 
+from constants import ANVIL_WALLET_PRIVATE_KEY, ARBITRUM
 from ipor_fusion.PlasmaVaultDataReader import PlasmaVaultDataReader
 from ipor_fusion.TransactionExecutor import TransactionExecutor
-from tests import constants
-from tests.constants import ANVIL_WALLET_PRIVATE_KEY
 
 
 def test_plasma_vault_data_reader():
@@ -14,18 +13,17 @@ def test_plasma_vault_data_reader():
     transaction_executor = TransactionExecutor(web3, ANVIL_WALLET_PRIVATE_KEY)
 
     plasma_vault_data = PlasmaVaultDataReader(transaction_executor).read(
-        constants.ARBITRUM.PILOT.SCHEDULED.PLASMA_VAULT
+        ARBITRUM.PILOT.SCHEDULED.PLASMA_VAULT
     )
 
     assert (
-        plasma_vault_data.plasma_vault_address
-        == constants.ARBITRUM.PILOT.SCHEDULED.PLASMA_VAULT
+        plasma_vault_data.plasma_vault_address == ARBITRUM.PILOT.SCHEDULED.PLASMA_VAULT
     )
     assert (
         plasma_vault_data.withdraw_manager_address
-        == constants.ARBITRUM.PILOT.SCHEDULED.WITHDRAW_MANAGER
+        == ARBITRUM.PILOT.SCHEDULED.WITHDRAW_MANAGER
     )
     assert (
         plasma_vault_data.access_manager_address
-        == constants.ARBITRUM.PILOT.SCHEDULED.ACCESS_MANAGER
+        == ARBITRUM.PILOT.SCHEDULED.ACCESS_MANAGER
     )
