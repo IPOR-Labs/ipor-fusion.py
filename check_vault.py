1f3fd6b08c701fdc212d04d0bce7d504988f9e83
amydlarz <amydlarz@gmail.com>
2024-11-28 15:18:41 +0100

    get_all_role_accounts

--- check_vault.py
+++ check_vault.py
@@ -1,7 +1,10 @@
 import logging
 import os
+from dataclasses import dataclass
+from typing import List
 
 from dotenv import load_dotenv
+from eth_typing import ChecksumAddress
 
 from ipor_fusion.PlasmaVaultSystemFactory import PlasmaVaultSystemFactory
 from ipor_fusion.Roles import Roles
@@ -16,6 +19,14 @@ plasma_vault_address = os.getenv("PLASMA_VAULT_ADDRESS")
 anvil_private_key = os.getenv("PRIVATE_KEY")
 
 
+@dataclass
+class RoleAccountGrouped:
+    account: ChecksumAddress
+    role_id: List[int]
+    is_member: bool
+    execution_delay: int
+
+
 def main():
     # setup
     system = PlasmaVaultSystemFactory(
@@ -23,15 +34,27 @@ def main():
         private_key=anvil_private_key,
     ).get(plasma_vault_address)
 
-    result = system.access_manager().get_accounts_with_roles(
-        [role.value for role in Roles]
-    )
+    result = system.access_manager().get_all_role_accounts()
+
+    unique_accounts = []
+    for role_account in result:
+        if not role_account.account in unique_accounts:
+            unique_accounts.append(role_account.account)
 
     with open(file="plasma_vault.md", mode="w", encoding="utf-8") as f:
-        for key, value in result.items():
-            f.write(f"{Roles.get_name(key)}({key}):\n")
-            for account in value:
-                f.write(f"- {account}\n")
+        for account in unique_accounts:
+            role_accounts = [
+                role_account
+                for role_account in result
+                if role_account.account == account
+            ]
+            f.write(f"{account} \n")
+            for role_account in role_accounts:
+                role_name = Roles.get_name(role_account.role_id).ljust(36)
+                role_id_text = f"role_id={role_account.role_id}".ljust(11)
+                f.write(
+                    f"- {role_name} ({role_id_text}, execution_delay={role_account.execution_delay}):\n"
+                )
             f.write("\n")
 
 
