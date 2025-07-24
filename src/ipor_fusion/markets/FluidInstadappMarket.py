7e18a2f6e552afb34b2bccfd06e5488247537666
amydlarz <adam@ipor.io>
2025-07-24 09:21:45 +0200

    [refactor] dependency injection for fuses

--- src/ipor_fusion/markets/FluidInstadappMarket.py
+++ src/ipor_fusion/markets/FluidInstadappMarket.py
@@ -4,9 +4,9 @@ from eth_typing import ChecksumAddress
 from web3 import Web3
 
 from ipor_fusion.ERC20 import ERC20
-from ipor_fusion.FuseMapper import FuseMapper
 from ipor_fusion.MarketId import MarketId
 from ipor_fusion.TransactionExecutor import TransactionExecutor
+from ipor_fusion.error.UnsupportedChainId import UnsupportedChainId
 from ipor_fusion.error.UnsupportedFuseError import UnsupportedFuseError
 from ipor_fusion.fuse.FluidInstadappSupplyFuse import FluidInstadappSupplyFuse
 from ipor_fusion.fuse.FuseAction import FuseAction
@@ -18,55 +18,50 @@ class FluidInstadappMarket:
         self,
         chain_id: int,
         transaction_executor: TransactionExecutor,
-        fuses: List[ChecksumAddress],
+        erc_4626_supply_fuse_market_id_5_address: ChecksumAddress,
+        fluid_instadapp_staking_supply_fuse_address: ChecksumAddress,
+        f_usdc_address: ChecksumAddress = None,
+        fluid_lending_staking_rewards_usdc_address: ChecksumAddress = None,
     ):
+        if transaction_executor is None:
+            raise ValueError("transaction_executor is required")
+
         self._chain_id = chain_id
         self._transaction_executor = transaction_executor
+        self._erc_4626_supply_fuse_market_id_5_address = (
+            erc_4626_supply_fuse_market_id_5_address
+        )
+        self._fluid_instadapp_staking_supply_fuse_address = (
+            fluid_instadapp_staking_supply_fuse_address
+        )
 
-        self._any_fuse_supported = False
-        for fuse in fuses:
-            checksum_fuse = Web3.to_checksum_address(fuse)
-            if checksum_fuse in FuseMapper.map(
-                chain_id=chain_id, fuse_name="Erc4626SupplyFuseMarketId5"
-            ):
-                self._fluid_instadapp_pool_fuse = FluidInstadappSupplyFuse(
-                    self.get_fUSDC(),
-                    checksum_fuse,
-                    self.get_FluidLendingStakingRewardsUsdc(),
-                    self.get_safe_fuse_address(
-                        chain_id, fuses, "FluidInstadappStakingSupplyFuse"
-                    ),
-                )
-                self._any_fuse_supported = True
+        self._f_usdc_address = f_usdc_address
+        if f_usdc_address is None:
+            self._f_usdc_address = self.get_fUSDC()
 
-        if self._any_fuse_supported:
-            self._pool = ERC20(
-                transaction_executor,
-                self.get_fUSDC(),
-            )
-            self._staking_pool = ERC20(
-                transaction_executor,
-                self.get_FluidLendingStakingRewardsUsdc(),
+        self._fluid_lending_staking_rewards_usdc_address = (
+            fluid_lending_staking_rewards_usdc_address
+        )
+        if fluid_lending_staking_rewards_usdc_address is None:
+            self._fluid_lending_staking_rewards_usdc_address = (
+                self.get_FluidLendingStakingRewardsUsdc()
             )
 
-    @staticmethod
-    def get_safe_fuse_address(
-        chain_id: int, vault_fuses: List[ChecksumAddress], fuse_name: str
-    ) -> ChecksumAddress:
-        fuses_from_mapper = FuseMapper.map(chain_id=chain_id, fuse_name=fuse_name)
-        fuse = None
-        for f in vault_fuses:
-            for x in fuses_from_mapper:
-                if f.lower() == x.lower():
-                    fuse = f
+        self._fluid_instadapp_pool_fuse = FluidInstadappSupplyFuse(
+            self._f_usdc_address,
+            self._erc_4626_supply_fuse_market_id_5_address,
+            self._fluid_lending_staking_rewards_usdc_address,
+            self._fluid_instadapp_staking_supply_fuse_address,
+        )
 
-        if not fuse:
-            raise UnsupportedFuseError()
-
-        return Web3.to_checksum_address(fuse)
-
-    def is_market_supported(self) -> bool:
-        return self._any_fuse_supported
+        self._pool = ERC20(
+            transaction_executor,
+            self._f_usdc_address,
+        )
+        self._staking_pool = ERC20(
+            transaction_executor,
+            self._fluid_lending_staking_rewards_usdc_address,
+        )
 
     def staking_pool(self) -> ERC20:
         return self._staking_pool
@@ -75,26 +70,26 @@ class FluidInstadappMarket:
         return self._pool
 
     def supply_and_stake(self, amount: int) -> List[FuseAction]:
-        if not hasattr(self, "_fluid_instadapp_pool_fuse"):
+        if self._fluid_instadapp_pool_fuse is None:
             raise UnsupportedFuseError(
                 "FluidInstadappSupplyFuse is not supported by PlasmaVault"
             )
 
         market_id = MarketId(
             FluidInstadappSupplyFuse.PROTOCOL_ID,
-            self.get_fUSDC(),
+            self._f_usdc_address,
         )
         return self._fluid_instadapp_pool_fuse.supply_and_stake(market_id, amount)
 
     def unstake_and_withdraw(self, amount: int) -> List[FuseAction]:
-        if not hasattr(self, "_fluid_instadapp_pool_fuse"):
+        if self._fluid_instadapp_pool_fuse is None:
             raise UnsupportedFuseError(
                 "FluidInstadappSupplyFuse is not supported by PlasmaVault"
             )
 
         market_id = MarketId(
             FluidInstadappSupplyFuse.PROTOCOL_ID,
-            self.get_fUSDC(),
+            self._f_usdc_address,
         )
         return self._fluid_instadapp_pool_fuse.unstake_and_withdraw(market_id, amount)
 
@@ -108,7 +103,7 @@ class FluidInstadappMarket:
                 "0xf42f5795D9ac7e9D757dB633D693cD548Cfd9169"
             )
 
-        raise BaseException("Chain ID not supported")
+        raise UnsupportedChainId("Chain ID not supported")
 
     def get_FluidLendingStakingRewardsUsdc(self) -> ChecksumAddress:
         if self._chain_id == 42161:
@@ -120,4 +115,4 @@ class FluidInstadappMarket:
                 "0x48f89d731C5e3b5BeE8235162FC2C639Ba62DB7d"
             )
 
-        raise BaseException("Chain ID not supported")
+        raise UnsupportedChainId("Chain ID not supported")
